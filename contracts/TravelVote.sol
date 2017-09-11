6ddfe06314d6a8c66003b57cc6e867184728f337
Wayne <wayne.5540@gmail.com>
2017-09-11 20:29:08 +0800

    restart

--- contracts/TravelVote.sol
+++ contracts/TravelVote.sol
@@ -2,58 +2,67 @@ pragma solidity ^0.4.15;
 
 contract TravelVote {
   address public owner;
-  bool public inProgress;
-  string public title;
-  address[] voters;
-  uint public agreementCount;
-  enum Results { Success, Failed, Even }
-  Results public result;
-
-  modifier onlyOwner {
-    require(msg.sender == owner);
-    _;
-  }
 
   function TravelVote() {
     owner = msg.sender;
-    inProgress = false;
   }
 
-  function start(string newTitle) onlyOwner {
-    require(inProgress != true);
-    inProgress = true;
-    title = newTitle;
-  }
 
-  function close() onlyOwner {
-    require(inProgress != false);
 
-    inProgress = false;
 
-    if (agreementCount == voters.length - agreementCount) {
-      result = Results.Even;
-    } else if (agreementCount > voters.length - agreementCount) {
-      result = Results.Success;
-    } else {
-      result = Results.Failed;
-    }
-  }
+  // address public owner;
+  // bool public inProgress;
+  // string public title;
+  // address[] voters;
+  // uint public agreementCount;
+  // enum Results { Success, Failed, Even }
+  // Results public result;
 
-  function getVoters() constant returns (address[]) {
-    return voters;
-  }
+  // modifier onlyOwner {
+  //   require(msg.sender == owner);
+  //   _;
+  // }
 
-  function vote(bool yes) {
-    for (uint index = 0; index < voters.length; index++) {
-      if (voters[index] == msg.sender) {
-        require(false);
-      }
-    }
+  // function TravelVote() {
+  //   owner = msg.sender;
+  //   inProgress = false;
+  // }
 
-    if (yes) {
-      agreementCount = agreementCount + 1;
-    }
+  // function start(string newTitle) onlyOwner {
+  //   require(inProgress != true);
+  //   inProgress = true;
+  //   title = newTitle;
+  // }
 
-    voters.push(msg.sender);
-  }
+  // function close() onlyOwner {
+  //   require(inProgress != false);
+
+  //   inProgress = false;
+
+  //   if (agreementCount == voters.length - agreementCount) {
+  //     result = Results.Even;
+  //   } else if (agreementCount > voters.length - agreementCount) {
+  //     result = Results.Success;
+  //   } else {
+  //     result = Results.Failed;
+  //   }
+  // }
+
+  // function getVoters() constant returns (address[]) {
+  //   return voters;
+  // }
+
+  // function vote(bool yes) {
+  //   for (uint index = 0; index < voters.length; index++) {
+  //     if (voters[index] == msg.sender) {
+  //       require(false);
+  //     }
+  //   }
+
+  //   if (yes) {
+  //     agreementCount = agreementCount + 1;
+  //   }
+
+  //   voters.push(msg.sender);
+  // }
 }
--- test/travel_vote.js
+++ test/travel_vote.js
@@ -4,16 +4,10 @@ import assertJump from './helpers/assertJump';
 
 contract('TravelVote', (accounts) => {
   let travelVote;
-  const resultEnum = {
-    Success: 0,
-    Failed: 1,
-    Even: 2
-  }
 
   beforeEach(async function () {
     travelVote = await TravelVote.new();
   });
-
 
   it("sets sender as owner", async () => {
     const owner = await travelVote.owner()
@@ -21,122 +15,142 @@ contract('TravelVote', (accounts) => {
     assert.equal(owner, accounts[0])
   })
 
-  describe("getVotors", () => {
-    it("return voters", async () => {
-      const voters = await travelVote.getVoters()
 
-      assert.isArray(voters)
-    })
-  })
 
-  describe('vote', () => {
-    it("adds voter", async () => {
-      await travelVote.vote(true)
-      const voters = await travelVote.getVoters()
+  // let travelVote;
+  // const resultEnum = {
+  //   Success: 0,
+  //   Failed: 1,
+  //   Even: 2
+  // }
 
-      assert.include(voters, accounts[0])
-    })
+  // beforeEach(async function () {
+  //   travelVote = await TravelVote.new();
+  // });
 
-    it("adds agreementCount if support", async () => {
-      await travelVote.vote(true)
-      const count = await travelVote.agreementCount()
 
-      assert.equal(count, 1)
-    })
+  // it("sets sender as owner", async () => {
+  //   const owner = await travelVote.owner()
 
-    it("doesn't adds agreementCount if support", async () => {
-      await travelVote.vote(false)
-      const count = await travelVote.agreementCount()
+  //   assert.equal(owner, accounts[0])
+  // })
 
-      assert.equal(count, 0)
-    })
+  // describe("getVotors", () => {
+  //   it("return voters", async () => {
+  //     const voters = await travelVote.getVoters()
 
-    it("can't vote more than once", async () => {
-      await travelVote.vote(true)
+  //     assert.isArray(voters)
+  //   })
+  // })
 
-      await expectThrow(
-        travelVote.vote(true)
-      )
-    })
-  })
+  // describe('vote', () => {
+  //   it("adds voter", async () => {
+  //     await travelVote.vote(true)
+  //     const voters = await travelVote.getVoters()
 
-  describe('close', () => {
-    const newTitle = "Restuarant in front of our office";
+  //     assert.include(voters, accounts[0])
+  //   })
 
-    beforeEach(async function () {
-      await travelVote.start(newTitle)
-    });
+  //   it("adds agreementCount if support", async () => {
+  //     await travelVote.vote(true)
+  //     const count = await travelVote.agreementCount()
 
-    it("toggles inProgress as false", async () => {
-      await travelVote.close()
-      const inProgress = await travelVote.inProgress()
+  //     assert.equal(count, 1)
+  //   })
 
-      assert.isFalse(inProgress)
-    })
+  //   it("doesn't adds agreementCount if support", async () => {
+  //     await travelVote.vote(false)
+  //     const count = await travelVote.agreementCount()
 
-    describe('sets result', () => {
-      it("even", async () => {
-        await travelVote.close()
-        const result = await travelVote.result()
+  //     assert.equal(count, 0)
+  //   })
 
-        assert.equal(result, resultEnum.Even)
-      })
+  //   it("can't vote more than once", async () => {
+  //     await travelVote.vote(true)
 
-      it("faied", async () => {
-        await travelVote.vote(false)
-        await travelVote.close()
-        const result = await travelVote.result()
+  //     await expectThrow(
+  //       travelVote.vote(true)
+  //     )
+  //   })
+  // })
 
-        assert.equal(result, resultEnum.Failed)
-      })
+  // describe('close', () => {
+  //   const newTitle = "Restuarant in front of our office";
 
-      it("success", async () => {
-        await travelVote.vote(true)
-        await travelVote.close()
-        const result = await travelVote.result()
+  //   beforeEach(async function () {
+  //     await travelVote.start(newTitle)
+  //   });
 
-        assert.equal(result, resultEnum.Success)
-      })
-    })
-  })
+  //   it("toggles inProgress as false", async () => {
+  //     await travelVote.close()
+  //     const inProgress = await travelVote.inProgress()
 
-  describe('start', () => {
-    const newTitle = "Restuarant in front of our office";
+  //     assert.isFalse(inProgress)
+  //   })
 
-    it("toggles inProgress as true", async () => {
-      await travelVote.start(newTitle)
-      const inProgress = await travelVote.inProgress()
+  //   describe('sets result', () => {
+  //     it("even", async () => {
+  //       await travelVote.close()
+  //       const result = await travelVote.result()
 
-      assert.isTrue(inProgress)
-    })
+  //       assert.equal(result, resultEnum.Even)
+  //     })
 
-    it("sets vote title", async () => {
-      await travelVote.start(newTitle)
-      const contractTitle = await travelVote.title()
+  //     it("faied", async () => {
+  //       await travelVote.vote(false)
+  //       await travelVote.close()
+  //       const result = await travelVote.result()
 
-      assert.equal(contractTitle, newTitle)
-    })
+  //       assert.equal(result, resultEnum.Failed)
+  //     })
 
-    it("only togglable when not in process", async () => {
-      await travelVote.start(newTitle)
+  //     it("success", async () => {
+  //       await travelVote.vote(true)
+  //       await travelVote.close()
+  //       const result = await travelVote.result()
 
-      await expectThrow(
-        travelVote.start(newTitle)
-      )
-    })
+  //       assert.equal(result, resultEnum.Success)
+  //     })
+  //   })
+  // })
 
-    describe('ownable', () => {
-      it('should prevent non-owners from transfering', async function () {
-        const other = accounts[2];
-        const owner = await travelVote.owner.call();
-        assert.isTrue(owner !== other);
-        try {
-          await travelVote.start(newTitle, { from: other });
-          assert.fail('should have thrown before');
-        } catch (error) {
-          assertJump(error);
-        }
-      });
-    })
-  })
+  // describe('start', () => {
+  //   const newTitle = "Restuarant in front of our office";
+
+  //   it("toggles inProgress as true", async () => {
+  //     await travelVote.start(newTitle)
+  //     const inProgress = await travelVote.inProgress()
+
+  //     assert.isTrue(inProgress)
+  //   })
+
+  //   it("sets vote title", async () => {
+  //     await travelVote.start(newTitle)
+  //     const contractTitle = await travelVote.title()
+
+  //     assert.equal(contractTitle, newTitle)
+  //   })
+
+  //   it("only togglable when not in process", async () => {
+  //     await travelVote.start(newTitle)
+
+  //     await expectThrow(
+  //       travelVote.start(newTitle)
+  //     )
+  //   })
+
+  //   describe('ownable', () => {
+  //     it('should prevent non-owners from transfering', async function () {
+  //       const other = accounts[2];
+  //       const owner = await travelVote.owner.call();
+  //       assert.isTrue(owner !== other);
+  //       try {
+  //         await travelVote.start(newTitle, { from: other });
+  //         assert.fail('should have thrown before');
+  //       } catch (error) {
+  //         assertJump(error);
+  //       }
+  //     });
+  //   })
+  // })
 })
