2851aec3a36852dd620ece480e08da4ea7730730
Wayne <wayne.5540@gmail.com>
2017-09-11 14:43:22 +0800

    us es6

--- test/lunch_vote.js
+++ test/lunch_vote.js
@@ -1,11 +1,11 @@
-var LunchVote = artifacts.require("./LunchVote.sol");
+const LunchVote = artifacts.require("./LunchVote.sol")
 
-contract('LunchVote', function (accounts) {
-  it("sets sender as owner", function () {
-    return LunchVote.deployed().then(function (instance) {
-      return instance.owner();
-    }).then(function(owner) {
-      assert.equal(owner, accounts[0]);
-    })
-  });
-});
+contract('LunchVote', (accounts) => {
+  it("sets sender as owner", () => {
+    LunchVote.deployed().then((instance) => (
+      instance.owner()
+    )).then((owner) => (
+      assert.equal(owner, accounts[0])
+    ))
+  })
+})
