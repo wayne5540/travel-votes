d39838bb1b4e82d6f752e12b57234fb68ef2f942
Wayne <wayne.5540@gmail.com>
2017-09-11 16:48:40 +0800

    setup babel

--- contracts/LunchVote.sol
+++ contracts/LunchVote.sol
@@ -2,8 +2,16 @@ pragma solidity ^0.4.15;
 
 contract LunchVote {
   address public owner;
+  address[] public voters;
+  bool public inProgress;
   
   function LunchVote() {
     owner = msg.sender;
+    inProgress = false;
+  }
+
+  function startVote() {
+    require(inProgress != true);
+    inProgress = true;
   }
 }
--- test/lunch_vote.js
+++ test/lunch_vote.js
@@ -1,11 +1,45 @@
 const LunchVote = artifacts.require("./LunchVote.sol")
+import expectThrow from './helpers/expectThrow';
 
 contract('LunchVote', (accounts) => {
-  it("sets sender as owner", () => {
-    LunchVote.deployed().then((instance) => (
-      instance.owner()
-    )).then((owner) => (
-      assert.equal(owner, accounts[0])
-    ))
-  })
+  it("sets sender as owner", async () => {
+    let instance = await LunchVote.deployed()
+    let owner = await instance.owner()
+
+    return assert.equal(owner, accounts[0])
+  }) 
+
+  // describe('startVote', () => {
+  //   it("toggles inProgress as true", () => {
+  //     return LunchVote.deployed().then((instance) => {
+  //       instance.startVote()
+  //       return instance.inProgress();
+  //     }).then((inProgress) => (
+  //       assert.equal(inProgress, true)
+  //     ))
+  //   })
+
+  //   it("only toggable when not in progress", () => {
+  //     return LunchVote.deployed().then((instance) => {
+  //       instance.startVote()
+  //       return instance;
+  //     }).then((instance) => {
+  //       instance.startVote()
+  //       return instance.inProgress();
+  //     }).then((inProgress) => (
+  //       assert.equal(inProgress, true)
+  //     ))
+  //   })
+  // })
+
+  // it("toggles inProgress as true", () => {
+  //   LunchVote.deployed().then((instance) => {
+  //     instance.startVote()
+  //     return instance;
+  //   }).then((instance) => (
+  //     instance.inProgress()
+  //   )).then((inProgress) => (
+  //     assert.equal("inProgress", "123")
+  //   ))
+  // })
 })
