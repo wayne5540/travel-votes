07a85b4a1ac5153b8738a5ea3393fc00cec5664e
Wayne <wayne.5540@gmail.com>
2017-09-14 20:36:16 +0800

    change back to stateless form

--- client/components/CreateProposalForm.jsx
+++ client/components/CreateProposalForm.jsx
@@ -4,53 +4,30 @@ import {
   Form,
   FormGroup,
   InputGroup,
-  FormControl,
-  HelpBlock
+  FormControl
 } from 'react-bootstrap';
 
-export default class CreateProposalForm extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+const CreateProposalForm = (props) => {
+  let input;
 
-    this.state = {
-      validationState: null,
-      inputValue: "",
-      helpText: ""
-    }
-  }
-
-  handleInputChange(event) {
-    this.setState({
-      inputValue: event.target.value,
-      validationState: (event.target.value ? "success" : "error"),
-      helpText: (event.target.value ? "" : "Can't be blank")
-    })
-  }
-
-  isValidInputValue() {
-    return this.state.inputValue ? true : false
-  }
-
-  render() {
-    return (
-      <Form onSubmit={(event) => { props.createProposalHandler(event, this.state.inputValue) }}>
-        <FormGroup validationState={this.state.validationState}>
-          <InputGroup>
-            <FormControl
-              type="text"
-              placeholder="Where do you want to go?"
-              value={this.state.inputValue}
-              onChange={this.handleInputChange.bind(this)}
-            />
-            <InputGroup.Button>
-              <Button type="submit" disabled={!this.isValidInputValue()} >
-                Propose
-              </Button>
-            </InputGroup.Button>
-          </InputGroup>
-          <HelpBlock>{this.state.helpText}</HelpBlock>
-        </FormGroup>
-      </Form>
-    )
-  }
+  return (
+    <Form onSubmit={(event) => { props.createProposalHandler(event, input.value) }}>
+      <FormGroup>
+        <InputGroup>
+          <FormControl
+            type="text"
+            placeholder="Where do you want to go?"
+            inputRef={node => input = node}
+          />
+          <InputGroup.Button>
+            <Button type="submit">
+              Propose
+            </Button>
+          </InputGroup.Button>
+        </InputGroup>
+      </FormGroup>
+    </Form>
+  )
 }
+
+export default CreateProposalForm
